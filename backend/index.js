5ee75c586c35bb61a69e255d098fdbac1bcc39e9
Aritra Baidik <aritrabaidik@gmail.com>
2023-11-29 11:15:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,22 @@ const cors = require('cors')
 const app = express()
 
 //cors
-app.use(cors({}))
+const allowedOrigins = [
+    "https://www.smartmaintenance.in",
+
+];
+
+app.use(cors({
+    origin:function(origin,callback){
+        if(allowedOrigins.indexOf(origin)!== -1 || !origin){
+            callback(null,true);
+        }
+        else{
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials:true,
+}))
 
 app.use(express.json())
 
