e47eade6b2c0fb6046c2995138dd88eb87d03504
Aritra Baidik <aritrabaidik@gmail.com>
2023-11-29 11:05:36 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -30,9 +30,7 @@ app.use(cors({
 
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Origin", "https://www.smartmaintenance.in");
     res.sendStatus(204);
     next()
 })
