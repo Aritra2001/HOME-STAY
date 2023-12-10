9f979db63c0e2a38c7da098ea338e51e1c251477
Aritra Baidik <aritrabaidik@gmail.com>
2023-12-10 22:45:13 +0530

    Update SignupVerified.jsx

--- frontend/src/Pages/SignupVerified.jsx
+++ frontend/src/Pages/SignupVerified.jsx
@@ -24,17 +24,22 @@ const SignupVerified = () => {
     }
   })
   json = await response.json()
+  console.log(json.email)
 
   }
   return (
     <div className='w-full h-screen relative items-center justify-center flex' onLoad={verifyEmail}>
         <div className="verified">
-            <img src={GreenTick} alt="Verified Green Tick" className='-mt-[15rem]' />
+            <img src={GreenTick} alt="Verified Green Tick" className='-mt-[2rem]' />
             <img src={Logo} alt="Home Stay Logo" className='mt-[3vh]'/>
-            <div className="w-[674px] h-12 text-black text-[45px] relative font-semibold font-['Poppins'] italic mt-[3vh]">
+            <div className="w-[674px] h-12 text-black text-[45px] font-semibold font-['Poppins'] italic mt-[3vh]">
                 Your email has been verified 
             </div>
-            <p>{json}</p>
+            <div className='text-black text-base font-regular font-["Poppins"] mt-[3rem]'>
+            Your email id <b>{json.email}</b> has been Verified!<br/>
+            You can now login with this email id.
+            </div>
+            <button className="w-[119px] h-[39px] bg-teal-600 rounded-[7px] text-white text-xl font-normal font-['Lexend'] mt-[3rem]">LOGIN</button>
         </div>
     </div>
   );
