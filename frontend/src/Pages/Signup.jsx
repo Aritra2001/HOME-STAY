f439c0110b759202e2692747c1ad08c9c7382506
Aritra Baidik <aritrabaidik@gmail.com>
2023-12-13 15:17:54 +0530

    Update Signup.jsx

--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -39,7 +39,7 @@ const Signup = () => {
 
     const signup = {email, password, confirmpassword}
 
-    const response = await fetch('http://home-stay-aritra2001.vercel.app/api/users/signup', {
+    const response = await fetch('https://home-stay-aritra2001.vercel.app/api/users/signup', {
 
       method: 'POST',
       body: JSON.stringify(signup),
