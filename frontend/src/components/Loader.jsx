14002ace797be61979cfa7b2c27a6f6abc81b0ba
Aritra Baidik <aritrabaidik@gmail.com>
2023-12-24 23:38:29 +0530

    logout button

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loader = () => {
   return (
-    <div className='flex inline justify-center align-center'>
+    <div className='flex inline-block justify-center align-center'>
       <div className='loader'></div>
       <span className='ml-[0.5rem]'>Loading</span>
     </div>
