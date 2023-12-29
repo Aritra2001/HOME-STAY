859e86321865ad14bb490c9e36decf45755e3680
Aritra Baidik <aritrabaidik@gmail.com>
2023-12-29 12:08:10 +0530

    css changes in home and admin page

--- frontend/src/Pages/Admin.jsx
+++ frontend/src/Pages/Admin.jsx
@@ -7,8 +7,8 @@ const Admin = () => {
 
   return (
     <div className='w-screen h-screen justify-center align-center'>
-      <nav>
-      <button onClick={logout} className="w-[100px] h-[37px] bg-teal-600 rounded-[7px] text-white text-xl font-semibold font-['Lexend'] mt-[4vh] ml-[80%] overflow-hidden max-sm:w-[80px] max-md:w-full max-sm:ml-[15rem] max-sm:font-normal max-sm:text-[15px]">Logout</button>
+      <nav className='-mt-3 bg-gray-200 pb-4'>
+      <button onClick={logout} className="w-[100px] h-[37px] bg-teal-600 rounded-[7px] text-white text-xl font-semibold font-['Lexend'] mt-[4vh] ml-[80%] overflow-hidden max-sm:w-[60px] max-sm:h-[29px] max-md:w-full max-sm:ml-[15rem] max-sm:font-normal max-sm:text-[12px]">Logout</button>
       </nav>
       <div className="p-5 font-normal font-['Lexend']">
         Admin Panel
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -5,10 +5,17 @@ const Home = () => {
 
   const { logout } = useLogout()
 
+  const handelNotification = () => {
+    Notification.requestPermission()
+    .then((perm) => {
+      alert(perm)
+    })
+  }
+
   return (
     <div className='w-full h-screen justify-center align-center'>
-      <nav>
-      <button onClick={logout} className="w-[100px] h-[37px] bg-teal-600 rounded-[7px] text-white text-xl font-semibold font-['Lexend'] mt-[4vh] ml-[80%] overflow-hidden max-sm:w-[80px] max-md:w-full max-sm:ml-[15rem] max-sm:font-normal max-sm:text-[15px]">Logout</button>
+      <nav className='-mt-3 bg-gray-200 pb-4'>
+      <button onClick={logout} className="w-[100px] h-[37px] bg-teal-600 rounded-[7px] text-white text-xl font-semibold font-['Lexend'] mt-[4vh] ml-[80%] overflow-hidden max-sm:w-[60px] max-sm:h-[29px] max-md:w-full max-sm:ml-[17rem] max-sm:font-normal max-sm:text-[12px]">Logout</button>
       </nav>
       <div className="p-5 font-normal font-['Lexend']">
         Home Page
